0ea992b848b1e62636bb8cd7f660c46081bbc70b
CelsonF <celsondev@outlook.com>
2025-07-22 22:59:20 -0300

    feat: create Two Sum Problem and Solved.

---
+++ leetcode/Main.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import leetcode.twoSum.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution twoSum = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum.twoSum(nums, target);
+        
+        if (result[0] != -1) {
+            System.out.println("Indices found: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No indices found that sum to the target.");
+        }
+
+
+        // Example of using the optimized method 
+        int[] optimizedResult = twoSum.twoSumOptimized(nums,target);
+        if (optimizedResult[0] != -1) {
+            System.out.println("Optimized indices found: " + optimizedResult[0] + ", " + optimizedResult[1]);
+        } else {
+            System.out.println("No indices found that sum to the target in optimized method.");
+        }
+    }
+}
---
+++ leetcode/twoSum/Solution.java
@@ -0,0 +1,49 @@
+package leetcode.twoSum;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+    /**
+     * Finds two indices in the array such that their values sum to the target.
+     *
+     * @param nums   the array of integers
+     * @param target the target sum
+     * @return an array containing the two indices, or {-1, -1} if no such indices
+     *         exist
+     */
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
+    /**
+     * Optimized version of the twoSum method using a HashMap to reduce time
+     * complexity.
+     *
+     * @param nums   the array of integers
+     * @param target the target sum
+     * @return an array containing the two indices, or {-1, -1} if no such indices
+     *         exist
+     */
+    public int[] twoSumOptimized(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
